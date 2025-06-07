fa5b2cfba32e3e0d5925c9621cfd6e549ecaf2fd
blackard967u <blackard967u@gmail.com>
2025-06-06 19:53:36 -0600

    04 tuplas

---
+++ 04_tuplas.py
@@ -0,0 +1,8 @@
+#funciona igual que las listas, pero en lugar de
+#corchetes se utilizan parentesis
+t = (1, 2, True, "Hola mundo")
+
+print(t)
+
+#imprime la clase de tipo que es "t", la cual es una tupla
+print(type(t))
