038b36452afc068eff84316ee19a10f1f9f0543c
blackard967u <blackard967u@gmail.com>
2025-06-06 20:00:56 -0600

    add info 04 tuplas

--- 04_tuplas.py
+++ 04_tuplas.py
@@ -6,3 +6,26 @@ print(t)
 
 #imprime la clase de tipo que es "t", la cual es una tupla
 print(type(t))
+
+#es necesario agregar al menos una coma para que sea considerada una tupla
+#sin coma
+t=(1)
+print(type(t))
+
+#con coma
+t = (1,)
+print(type(t))
+
+#utilizamos [] para referirnos a un elemento de una tupla
+
+t = ("one", "two", "three", "four", "five", "six")
+print(t[2])
+
+##Las tuplas y las listas forman parte de un tipo de objetos llamados
+#  SECUENCIAS, así que también podemos hacer cosas 
+#como el slicing
+
+print(t[2:4])
+
+##Las tuplas no poseen mecanismos de modificación
+
