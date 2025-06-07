636810127bb14c8542461b79fd3ac02ab4b19b81
blackard967u <blackard967u@gmail.com>
2025-06-06 19:52:33 -0600

    02 cadenas

---
+++ 02_cadenas.py
@@ -0,0 +1,18 @@
+#cadena simple
+cadena = "cadena"
+
+print(cadena)
+#cadena triple, para mostrar en varias  lineas
+triple = """primera linea, 
+otra linea, 
+y otra linea"""
+
+print(triple)
+#suma de cadenas
+sum_cadena = "Hola "+"Mundo" 
+
+print(sum_cadena)
+#multiplicacion de cadena, repite  la cadena las veces solicitadas
+mult_cadena = "Hola mundo"*3
+
+print(mult_cadena)
