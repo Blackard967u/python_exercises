76cd0735d72e0b99d7c120ed5048fe73fa6b10c5
Aurora Carmona <u746221@ext.bancosabadell.mx>
2025-06-06 19:48:12 -0600

    01 Hola Mundo

---
+++ 01_operadores_aritmeticos.py
@@ -0,0 +1,36 @@
+print("Operadores")
+
+num1 = 234 
+num2 = 434
+#suma
+sum = num1 + num2
+
+print("suma = "+str(sum))
+#resta
+rest = num1 - num2
+
+print(rest)
+#negacion
+negac = -num1
+
+print(negac)
+#multiplicacion
+mult = num1 * num2
+
+print(mult)
+#exponente
+expo = num1 ** num2
+
+print(expo)
+#division
+div = num1 / num2
+
+print(div)
+#division_entera
+div_ent = num1 // num2
+
+print(div_ent)
+#modulo
+mod = num1 % num2
+
+print(mod)
