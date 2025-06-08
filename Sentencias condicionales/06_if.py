566b8aaf102ffd5577b0e90572079aa2139838a5
blackard967u <blackard967u@gmail.com>
2025-06-07 23:14:44 -0600

    06 If

---
+++ Sentencias condicionales/06_if.py
@@ -0,0 +1,10 @@
+
+a="Hola"
+#Es importante identar la sentencia para que python
+#pueda determinar que se trata de un if
+if a == "Hola":
+    print("Hola, mucho gusto")
+
+if a == "Hola":
+    print("Hola, mucho gusto")
+print("Linea a parte")
