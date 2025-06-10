959da3756c7addf60766192ba6200d0ee16a82f1
blackard967u <blackard967u@gmail.com>
2025-06-10 15:20:16 -0600

    05 Diccionarios métodos comunes

--- 05_diccionarios.py
+++ 05_diccionarios.py
@@ -17,3 +17,66 @@ print(music_d["Avicci"])
 # En los diccionarios NO podemos utilizar slicing, 
 #pero podemos utilizar mappings
 
+
+
+
+
+
+
+
+
+
+
+#------------------------------------Métodos útiles de los diccionarios---------------------------------------
+
+a = music_d.get('Martin Garrix', "No especificado")
+
+print(a)
+
+# El primer parametro es la clave a buscar dentro del diccionario, el segundo parámetro
+# es el valor que devolverá si no se encuentra esa clave, es decir el valor por defecto.
+
+print("--------------------------------------------------")
+
+a = "Avicci" in music_d
+
+print(a)
+
+# Comprueba si el diccionario tiene la clave específicada y devuelve un valor booleano
+
+print("--------------------------------------------------")
+
+
+a = music_d.items()
+
+print(a)
+
+#  Devuelve una lista tuplas con pares clave-valor
+
+print("--------------------------------------------------")
+
+a = music_d.keys()
+
+print(a)
+
+# Devuelve una lista de las claves del diccionario
+
+print("--------------------------------------------------")
+
+a = music_d.values()
+
+print(a)
+
+# Devuelve una lista de los valores del diccionario
+
+print("--------------------------------------------------")
+
+
+a = music_d.pop('Avicci', "No especificado")
+
+print(a)
+print(music_d)
+
+# Elimina el valor del diccionario y devuelve el valor de la clave
+
+
